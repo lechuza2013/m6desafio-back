e1d0761e12a6b6b8f7c66531a7907458ba573bf3
axel <julian_s_99@live.com>
2023-03-14 13:10:26 -0300

    checking back

---
+++ db.js
@@ -0,0 +1,29 @@
+import * as admin from "firebase-admin";
+import { initializeApp } from "firebase-admin";
+import { getDatabase } from "firebase/database";
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_MAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_CERT,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+};
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL
+};
+const app = initializeApp(firebaseConfig);
+const rtdb = getDatabase(app);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DB_URL
+});
+const firestore = admin.firestore();
+export { firestore, rtdb };
