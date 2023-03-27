2b19a1d5763fe8830e345d1da50447a4d87432d4
axel <julian_s_99@live.com>
2023-03-27 14:33:32 -0300

    private_key parsed

--- server/db.ts
+++ server/db.ts
@@ -6,7 +6,7 @@ const serviceAccount = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
     client_email: process.env.FIREBASE_CLIENT_MAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
