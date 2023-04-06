536b9887040540b3b724e03582a2cbfa2bcc6a99
axel <julian_s_99@live.com>
2023-04-06 15:38:22 -0300

    back completado al 91%

--- server/db.ts
+++ server/db.ts
@@ -1,23 +1,22 @@
 import admin from "firebase-admin";
 
-import * as serviceAccount from "../key.json";
-// const serviceAccount = {
-//     type: "service_account",
-//     project_id: process.env.FIREBASE_PROJECT_ID,
-//     private_key_id: process.env.FIREBASE_KEY_ID,
-//     private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
-//     client_email: process.env.FIREBASE_CLIENT_MAIL,
-//     client_id: process.env.FIREBASE_CLIENT_ID,
-//     auth_uri: process.env.FIREBASE_AUTH_URI,
-//     token_uri: process.env.FIREBASE_TOKEN_URI,
-//     auth_provider_x509_cert_url: process.env.FIREBASE_CERT,
-//     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
-// }
+// import * as serviceAccount from "../key.json";
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_KEY_ID,
+  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
+  client_email: process.env.FIREBASE_CLIENT_MAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_CERT,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: "https://probandoapx-default-rtdb.firebaseio.com/",
-  // databaseURL: process.env.FIREBASE_DB_URL,
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 
 const firestoreDB = admin.firestore();
