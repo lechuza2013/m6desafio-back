5704071c3f0e793932e12595de62fdc4693a2101
axel <julian_s_99@live.com>
2023-05-05 19:11:13 -0300

    package modifications v3

--- server/db.ts
+++ server/db.ts
@@ -8,9 +8,9 @@ const serviceAccount = {
   private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
   client_email: process.env.FIREBASE_CLIENT_MAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_CERT,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://accounts.google.com/o/oauth2/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
 };
 
