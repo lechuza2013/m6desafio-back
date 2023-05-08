f827e407cb838829791684af4d490894f65fa87d
axel <julian_s_99@live.com>
2023-05-08 19:39:07 -0300

    serviceAccount updated v1

--- server/db.ts
+++ server/db.ts
@@ -1,23 +1,7 @@
 import admin from "firebase-admin";
 
 // import * as serviceAccount from "../key.json";
-const serviceAccount = {
-  type: "service_account",
-  apiKey: process.env.FIREBASE_API_KEY,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_KEY_ID,
-  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
-  client_email: process.env.FIREBASE_CLIENT_MAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_CERT,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
