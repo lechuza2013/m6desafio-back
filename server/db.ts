61f9129d62a66b5d389719cd7ff2a23ecfdc1ab5
axel <julian_s_99@live.com>
2023-05-22 09:30:44 -0300

    getRoomsId updated v1.7

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,14 @@
 import admin from "firebase-admin";
 
-// import * as serviceAccount from "../key.json";
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+import * as serviceAccount from "../key.json";
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: process.env.FIREBASE_DB_URL,
+  databaseURL: "https://probandoapx-default-rtdb.firebaseio.com",
 });
+
+// process.env.FIREBASE_DB_URL
 
 const firestoreDB = admin.firestore();
 const realtimeDB = admin.database();
